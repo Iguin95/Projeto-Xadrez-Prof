0a3bdae7836efc8669e7a9b3b6d4f911c07b7c2b
Igor Vinnícius <igorquintao22@gmail.com>
2023-11-28 22:08:10 -0300

    Iniciando a colocação das peças no tabuleiro

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -9,7 +9,7 @@ public class Board {
 	public Board(int rows, int columns) {
 		this.rows = rows;
 		this.columns = columns;
-		pieces = new Piece[rows][columns];
+		pieces = new Piece[rows][columns]; //é uma obrigatoriedade a criação de uma matriz da peça, ou seja, assim que o tabuleiro (Board) é criado, a peça recebe uma posição
 	}
 
 	public int getRows() {
@@ -36,5 +36,9 @@ public class Board {
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
+	public void placePiece(Piece piece, Position position) { //método resposável por colocar a peça de Xadrez no devido lugar selecionado. O método está recebendo(nos seus argumentos), uma pela e uma posição
+		pieces[position.getRow()][position.getColumn()] = piece; //atribuir a posição dada na matriz 'pieces', a peça informada. Essa matriz é chamada no construtor desta classe 
+		piece.position = position;//no caso, tenho que informar que a peça existe, ou seja, não está em uma posição nula. Então, graças a visibilidade 'protected' do atributo 'position' da classe Piece, posso atribuir a sua posição a posição informada nesse método através do argumento 'position' dentro desse escopo
+	}
 
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,8 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 	
@@ -8,16 +10,21 @@ public class ChessMatch {
 	
 	public ChessMatch() {
 		board = new Board(8, 8); //quem tem que saber a dimensão de um tabuleiro de Xadrez, é classe resposável pelas regras do jogo, que no caso é essa. Dimensão do tabuleiro é 8x8.
+		initialSetup();//quando se é criada uma nova partida de Xadrez (ChessMatch), se cria um tabuleiro 8x8 e chama o método InitialSetup().
 	}
 	
 	public ChessPiece[][] getPieces(){ //método para retornar uma matriz de peças de Xadrez correspondentes a esta partida.
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()]; //eu estou apenas mostrando a peça de Xadrez (chessPiece) para que meu programa conheça somente a camada de Xadrez, e não a de Tabuleiro, onde contém a peça(piece). Dentro da matriz está a quantidade de linhas e colunas do tabuleiro
 		for(int i = 0; i < board.getRows();i++) {
 			for(int j = 0;j < board.getColumns();j++) {
-				mat[i][j] = (ChessPiece) board.piece(i, j);
+				mat[i][j] = (ChessPiece) board.piece(i, j);//para cada peça do meu tabuleiro, eu vou fazer um downcasting para 'chesspiece'
 			}
 		}
 		return mat;
+	}
+	
+	private void initialSetup() { //método resposável por iniciar a partida de Xadrez, colocando as peças no tabuleiro
+		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
 	}
 
 }
