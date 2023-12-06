1c4ff70ccd5dc7c8007a633b3b845c3ac22b1df3
Igor Vinnícius <igorquintao22@gmail.com>
2023-12-06 14:30:23 -0300

    Tratando as exceções e limpando a tela

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -14,16 +16,27 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch(); //instancia a partida de xadrez
 
 		while (true) {
-			UI.printBoard(chessMatch.getPieces());//instancia o interface do tabuleiro
-			System.out.println();
-			System.out.print("Source: ");
-			ChessPosition source = UI.readChessPosition(sc); //vai ler do usuário a posição de origem
-
-			System.out.println();
-			System.out.print("Target: ");
-			ChessPosition target = UI.readChessPosition(sc); //vai ler do usuário a posição de destino
-
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target); //vai mover da origem para o destino
+			try {
+				UI.clearScreen(); //a cada vez que rodar o loop, a tela será limpa
+				UI.printBoard(chessMatch.getPieces());//instancia o interface do tabuleiro
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc); //vai ler do usuário a posição de origem
+	
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc); //vai ler do usuário a posição de destino
+	
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target); //vai mover da origem para o destino
+			}
+			catch(ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch(InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
 
 		
